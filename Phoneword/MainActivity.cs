af7c542e13af910ea9ba30bc433fdb87920dcdf5
Lernen & Arbeiten <Evendur@live.de>
2016-10-10 20:31:32 +0200

    Complete Version 1.0

--- Phoneword/MainActivity.cs
+++ Phoneword/MainActivity.cs
@@ -1,4 +1,8 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
 
@@ -9,10 +13,56 @@ namespace Phoneword
     {
         protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(bundle);
+            base.OnCreate (bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            // Our Code will go here
+
+            //Get our UI Controls from the loaded Layout
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            //Disable the "Call" button 
+            callButton.Enabled = false;
+
+            // Add Code to translate number
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                //Translate user`s alphanumeric phone number to numeric
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                if (String.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call" + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                // On Call button click try to dial phone number.
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call" + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    // Create intend to dial phone
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // Show the alert dialog to the user and wait for response.
+                callDialog.Show();
+            };
         }
     }
 }
